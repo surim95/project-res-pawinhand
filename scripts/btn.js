f8a2be62e381a762ff90e2e06166be20c939feac
surim95 <cheasulim2731@gmail.com>
2025-06-30 15:53:54 +0900

    1,2행 제외 구성 완성

--- scripts/btn.js
+++ scripts/btn.js
@@ -1,3 +1,4 @@
+/* 헤더 광고 닫기 버튼 */
 const closeAdBtn = document.querySelector('.ad_close_btn');
 const headerAd = document.querySelector('.ad');
 let adif = false;
@@ -11,3 +12,10 @@ closeAdBtn.addEventListener('click',()=>{
         adif = false;
     }
 });
+
+/* 더보기 버튼 복사/붙여넣기 */
+const moreBtn = document.querySelector('.pet_sup .more_b_btn');
+const reviewMore = document.querySelector('.review_container .review_wrap');
+const moreBtnClone = moreBtn.cloneNode(true);
+console.log(moreBtn, reviewMore, moreBtnClone);
+reviewMore.appendChild(moreBtnClone);
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -7,3 +7,19 @@ const mainBnr = new Swiper('.main_bnr_swiper',{
         type: 'bullets',
     },
 });
+const magazineScroll = new Swiper('.magazine_scroll',{
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    navigation:{
+        nextEl:'.magazine_swiper_wrap > .swiper-button-next',
+        prevEl:'.magazine_swiper_wrap > .swiper-button-prev',
+    },
+    autoplay:{delay:3000},
+})
+const introduceScroll = new Swiper('.intro_pic',{
+    navigation:{
+        nextEl:'.intro_pic > .swiper-button-next',
+        prevEl:'.intro_pic > .swiper-button-prev',
+    }, loop:true,
+    autoplay:{delay:3000},
+})
