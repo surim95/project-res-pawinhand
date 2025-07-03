a3a18263e6f15c95591cfd68986cb2f8c93821b4
surim95 <cheasulim2731@gmail.com>
2025-07-03 09:58:02 +0900

    포인핸드 detail

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,6 +1,6 @@
 //main bnr 스크롤
 const mainBnr = new Swiper('.main_bnr_swiper',{
-    /* autoplay:{delay:3000}, */
+    autoplay:{delay:3000},
     loop:true,
     pagination:{
         el:'.main_bnr_swiper .swiper-pagination',
@@ -14,18 +14,18 @@ const magazineScroll = new Swiper('.magazine_scroll',{
         nextEl:'.magazine_swiper_wrap > .swiper-button-next',
         prevEl:'.magazine_swiper_wrap > .swiper-button-prev',
     },
-    /* autoplay:{delay:3000}, */
+    autoplay:{delay:3000},
 })
 const introduceScroll = new Swiper('.intro_pic',{
     navigation:{
         nextEl:'.intro_pic > .swiper-button-next',
         prevEl:'.intro_pic > .swiper-button-prev',
     }, loop:true,
-    /* autoplay:{delay:3000}, */
+    autoplay:{delay:3000},
 })
 const bestScroll = new Swiper('.best_scroll',{
     slidesPerView: 'auto',
-    /* autoplay:{delay:3000}, */
+    autoplay:{delay:3000},
     loop:true,
     navigation:{
         nextEl:'.best_container .swiper-button-next',
@@ -37,9 +37,9 @@ const bestScroll = new Swiper('.best_scroll',{
 })
 const supportScroll = new Swiper('.support_scroll',{
     slidesPerView: 'auto',
-    /* autoplay:{delay:3000,
+    autoplay:{delay:3000,
         reverseDirection:true,
-    }, */
+    },
     loop:true,
     navigation:{
         nextEl:'.support_container .swiper-button-next',
