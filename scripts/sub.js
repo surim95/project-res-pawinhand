139bf3a0cc3e0bc97012fdbd3af60791cfa2d707
surim95 <cheasulim2731@gmail.com>
2025-07-04 17:14:37 +0900

    서브페이지 작업

--- scripts/sub.js
+++ scripts/sub.js
@@ -5,7 +5,6 @@ document.querySelectorAll('a').forEach(a => {
         }
     });
 });
-
 /* 헤더 광고 닫기 버튼 */
 const closeAdBtn = document.querySelector('.ad_close_btn');
 const headerAd = document.querySelector('.ad');
@@ -13,14 +12,6 @@ console.log(closeAdBtn, headerAd)
 closeAdBtn.addEventListener('click',()=>{
     headerAd.style.display = 'none';
 });
-
-/* 더보기 버튼 복사/붙여넣기 */
-const moreBtn = document.querySelector('.pet_sup .more_b_btn');
-const reviewMore = document.querySelector('.review_container .review_wrap');
-const moreBtnClone = moreBtn.cloneNode(true);
-console.log(moreBtn, reviewMore, moreBtnClone);
-reviewMore.appendChild(moreBtnClone);
-
 /* 장바구니, 검색, 마이페이지 활성화 */
 const searchIcon = document.querySelector('.search img');
 const cartIcon = document.querySelector('.cart img');
@@ -66,23 +57,6 @@ cartPlusBtn.forEach(btn => {
     })
 })
 
-/* 요소 개별 background 변경 */
-const magazineBtn = document.querySelector('.magazine_contents .more_info_btn');
-magazineBtn.addEventListener('mouseover',()=>{
-    magazineBtn.children[0].src = './images/icon_arrow_more_act.png'
-})
-magazineBtn.addEventListener('mouseout',()=>{
-    magazineBtn.children[0].src = './images/icon_arrow_more.png'
-})
-
-const infoBtn = document.querySelector('.more_brand_btn');
-infoBtn.addEventListener('mouseover',()=>{
-    infoBtn.children[0].src = './images/icon_arrow_more_act.png'
-})
-infoBtn.addEventListener('mouseout',()=>{
-    infoBtn.children[0].src = './images/icon_arrow_more.png'
-})
-
 const titles = document.querySelectorAll('.lnb_title');
 
 titles.forEach(title => {
@@ -94,3 +68,63 @@ titles.forEach(title => {
     img.src = './images/icon_next_arrow.png'; // 마우스 아웃 시 원래 이미지로 복귀
     });
 });
+
+/* sub */
+const priceSale = document.querySelectorAll('.product_info p');
+
+priceSale.forEach(filter => {
+    const sale = filter.querySelector('.sale');
+    const price = filter.querySelector('.price');
+
+    if (sale && price) {
+        price.classList.add('del_price');
+    } /* else {
+        console.log('없음');
+    } */
+})
+
+const filterAct = document.querySelectorAll('.filter_list');
+
+filterAct.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+        filterAct.forEach(btn => {
+            if(btn.id === 'filter_active') {
+                btn.removeAttribute('id');
+            }
+        });
+        filterBtn.id = 'filter_active';
+    });
+});
+
+const page = document.querySelectorAll('.page');
+
+page.forEach(pageBtn => {
+    pageBtn.addEventListener('click', () => {
+        page.forEach(btn => {
+            if(btn.id === 'page_active') {
+                btn.removeAttribute('id');
+            }
+        });
+        pageBtn.id = 'page_active';
+    });
+});
+
+const alignFour = document.querySelector('.list_show4');
+const alignOne = document.querySelector('.list_show1');
+
+alignFour.addEventListener('click',()=>{
+    alignFour.children[0].src = '../images/sub/icon_align4_btn_act.png';
+    alignOne.children[0].src = '../images/sub/icon_align1_btn.png';
+});
+alignOne.addEventListener('click',()=>{
+    alignFour.children[0].src = '../images/sub/icon_align4_btn.png';
+    alignOne.children[0].src = '../images/sub/icon_align1_btn_act.png';
+});
+
+/* ul 복사 붙여넣기 */
+const productWrap = document.querySelector('.product_wrap');
+const productList = document.querySelector('.product_list');
+const productListClone = productList.cloneNode(true);
+for (let i = 1; i<3; i++){
+    productWrap.appendChild(productListClone);
+}
